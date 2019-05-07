5159f3ed4a21957cfad2e9513b06304c6e98f4c1
Allard Buijze <allard.buijze@axoniq.io>
2019-05-07 11:36:17 +0200

    Initial commit - empty project

---
+++ src/main/java/io/axoniq/demo/bikerental/bikerental/BikerentalApplication.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.bikerental.bikerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/axoniq/demo/bikerental/bikerental/BikerentalApplicationTests.java
@@ -0,0 +1,16 @@
+package io.axoniq.demo.bikerental.bikerental;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BikerentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
