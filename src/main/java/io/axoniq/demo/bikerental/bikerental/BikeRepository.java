a53b8a7825d5e22e0f2aacf867811fb7ad0db042
Allard Buijze <allard.buijze@axoniq.io>
2019-05-07 14:42:09 +0200

    Step 2 - Splitting commands from queries, synced using events

--- src/main/java/io/axoniq/demo/bikerental/bikerental/BikeRepository.java
+++
@@ -1,8 +0,0 @@
-package io.axoniq.demo.bikerental.bikerental;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("bikeQueryRepository")
-public interface BikeRepository extends JpaRepository<Bike, String> {
-}
---
+++ src/main/java/io/axoniq/demo/bikerental/bikerental/query/BikeStatusProjection.java
@@ -0,0 +1,50 @@
+package io.axoniq.demo.bikerental.bikerental.query;
+
+import io.axoniq.demo.bikerental.bikerental.coreapi.BikeRegisteredEvent;
+import io.axoniq.demo.bikerental.bikerental.coreapi.BikeRentedEvent;
+import io.axoniq.demo.bikerental.bikerental.coreapi.BikeReturnedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BikeStatusProjection {
+
+    private final BikeStatusRepository bikeStatusRepository;
+
+    public BikeStatusProjection(BikeStatusRepository bikeStatusRepository) {
+        this.bikeStatusRepository = bikeStatusRepository;
+    }
+
+    @EventHandler
+    public void on(BikeRegisteredEvent event) {
+        bikeStatusRepository.save(new BikeStatus(event.getBikeId(), event.getLocation()));
+    }
+
+    @EventHandler
+    public void on(BikeRentedEvent event) {
+        bikeStatusRepository.findById(event.getBikeId()).map(bs -> {
+            bs.setRenter(event.getRenter());
+            return bs;
+        });
+    }
+
+    @EventHandler
+    public void on(BikeReturnedEvent event) {
+        bikeStatusRepository.findById(event.getBikeId()).map(bs -> {
+            bs.setRenter(null);
+            bs.setLocation(event.getLocation());
+            return bs;
+        });
+    }
+
+    @QueryHandler(queryName = "findAll")
+    public Iterable<BikeStatus> findAll() {
+        return bikeStatusRepository.findAll();
+    }
+
+    @QueryHandler(queryName = "findOne")
+    public BikeStatus findOne(String bikeId) {
+        return bikeStatusRepository.findById(bikeId).orElse(null);
+    }
+}
