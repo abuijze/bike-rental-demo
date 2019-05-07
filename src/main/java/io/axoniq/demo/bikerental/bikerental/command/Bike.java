301e741216b010ffcb1528d43ea0d10be8367dcb
Allard Buijze <allard.buijze@axoniq.io>
2019-05-07 15:13:57 +0200

    Step 3 - Implement Event Sourcing using AxonServer for Bike Aggregate

--- src/main/java/io/axoniq/demo/bikerental/bikerental/command/Bike.java
+++ src/main/java/io/axoniq/demo/bikerental/bikerental/command/Bike.java
@@ -2,22 +2,18 @@ package io.axoniq.demo.bikerental.bikerental.command;
 
 import io.axoniq.demo.bikerental.bikerental.coreapi.*;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
-
-import javax.persistence.Basic;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
-@Entity
 @Aggregate
 public class Bike {
 
-    @Id
+    @AggregateIdentifier
     private String bikeId;
 
-    @Basic
     private boolean isAvailable;
 
     public Bike() {
@@ -25,8 +21,6 @@ public class Bike {
 
     @CommandHandler
     public Bike(RegisterBikeCommand command) {
-        this.bikeId = command.getBikeId();
-        this.isAvailable = true;
         apply(new BikeRegisteredEvent(command.getBikeId(), command.getLocation()));
     }
 
@@ -35,7 +29,6 @@ public class Bike {
         if (!this.isAvailable) {
             throw new IllegalStateException("Bike is already rented");
         }
-        this.isAvailable = false;
         apply(new BikeRentedEvent(command.getBikeId(), command.getRenter()));
     }
 
@@ -44,7 +37,22 @@ public class Bike {
         if (this.isAvailable) {
             throw new IllegalStateException("Bike was already returned");
         }
-        this.isAvailable = true;
         apply(new BikeReturnedEvent(command.getBikeId(), command.getLocation()));
+    }
+
+    @EventSourcingHandler
+    protected void handle(BikeRegisteredEvent event) {
+        this.bikeId = event.getBikeId();
+        this.isAvailable = true;
+    }
+
+    @EventSourcingHandler
+    protected void handle(BikeReturnedEvent event) {
+        this.isAvailable = true;
+    }
+
+    @EventSourcingHandler
+    protected void handle(BikeRentedEvent event) {
+        this.isAvailable = false;
     }
 }
