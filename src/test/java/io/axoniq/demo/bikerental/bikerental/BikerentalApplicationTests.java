4b625491b945f92ff3b202e6249c8c0c3d6ec2f9
Allard Buijze <allard.buijze@axoniq.io>
2019-10-14 16:02:57 +0200

    Updated versions of dependencies
    Updated to Spring Boot 2.5, Axon 4.5 and Junit 5

--- src/test/java/io/axoniq/demo/bikerental/bikerental/BikerentalApplicationTests.java
+++ src/test/java/io/axoniq/demo/bikerental/bikerental/BikerentalApplicationTests.java
@@ -1,16 +1,13 @@
 package io.axoniq.demo.bikerental.bikerental;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BikerentalApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
